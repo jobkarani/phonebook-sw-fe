9624990a2ef8f51f20e6bd8f04c153fbeea8b2f4
Job Karani <jobkarani084j@gmail.com>
2023-03-01 08:34:25 +0300

    feat: creates and updates contact service

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,10 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Contact } from '../interfaces/contact';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
+  baseUrl = 'http://127.0.0.1:8000/contacts/';
+
+  constructor(private http: HttpClient) { }
+
+  getContacts(): Observable<Contact[]> {
+    return this.http.get<Contact[]>(this.baseUrl);
+  }
+
+  createContact(contact: Contact): Observable<Contact> {
+    return this.http.post<Contact>(`${this.baseUrl}create/`, contact);
+  }
+
+  getContact(id: number): Observable<Contact> {
+    return this.http.get<Contact>(`${this.baseUrl}${id}/`);
+  }
+
+  updateContact(id: number, contact: Contact): Observable<Contact> {
+    return this.http.put<Contact>(`${this.baseUrl}${id}/update/`, contact);
+  }
+
+  deleteContact(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}${id}/delete/`);
+  }
   
 }
